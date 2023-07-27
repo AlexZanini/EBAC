73f8933e01c610a33db242e9a0beb11f4940334a
Alex Zanini <97163043+AlexZanini@users.noreply.github.com>
2023-07-27 11:40:06 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+"use strict";
+// Função de multiplicação
+function multiplicar(num1, num2) {
+    return num1 * num2;
+}
+// Função de saudação
+function saudar(nome) {
+    return `Olá ${nome}`;
+}
+// Exemplos de uso
+let resultado = multiplicar(3, 4); // Retorna 12
+console.log(resultado);
+let saudacao = saudar("Alex Zanini"); // Retorna "Olá Alex Zanini"
+console.log(saudacao);
---
+++ index.ts
@@ -0,0 +1,16 @@
+// Função de multiplicação
+function multiplicar(num1: number, num2: number): number {
+    return num1 * num2;
+}
+
+// Função de saudação
+function saudar(nome: string): string {
+    return `Olá ${nome}`;
+}
+
+// Exemplos de uso
+let resultado = multiplicar(3, 4);  // Retorna 12
+console.log(resultado);
+
+let saudacao = saudar("Alex Zanini");  // Retorna "Olá Alex Zanini"
+console.log(saudacao);
